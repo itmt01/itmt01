ae7be5e52f80b18311e28deab6bc76f2875d960b
itmt01 <59216913+itmt01@users.noreply.github.com>
2022-11-22 20:27:04 +0700

    Update revenuecat.js

--- revenuecat.js
+++ revenuecat.js
@@ -5,7 +5,9 @@ const ua = $request.headers['User-Agent'] || $request.headers['user-agent'];
 const list = {
 	'VSCO': { name: 'membership', id: 'com.circles.fin.premium.yearly' },
 	'1Blocker': { name: 'premium', id: 'blocker.ios.subscription.yearly' },
-	'Anybox': { name: 'pro', id: 'cc.anybox.Anybox.annual' }
+	'Anybox': { name: 'pro', id: 'cc.anybox.Anybox.annual' },
+	'Fileball': { name: 'filebox_pro', id: 'com.premium.yearly' },
+	'ipTV': { name: 'ipTV +', id: 'iptv_9.99_1y_7d_free' }
 };
 const data = {
 	"expires_date": "2099-02-18T07:52:54Z",
